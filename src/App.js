094ef25c81a040acab770c8412ac5737a8066cac
sevilkaragozyilmaz <sevilkaragozz@gmail.com>
2023-04-17 00:15:32 +0300

    project first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import './HomePage.js';
+import './AboutUs';
+import HomePage from './HomePage.js';
+import AboutUs from './AboutUs.js';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+        <Route exact path='/' element={<HomePage />}/>
+        <Route path='AboutUs' element={<AboutUs />}/>
+        </Routes>
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
